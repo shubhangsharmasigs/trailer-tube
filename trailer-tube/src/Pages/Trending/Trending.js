cccfb55220cbf96d97114607089c0fadbfa1697b
shubhangsharmasigs <shubhangsharmasigs@gmail.com>
2022-02-02 02:27:43 +0530

    pagination

--- trailer-tube/src/Pages/Trending/Trending.js
+++ trailer-tube/src/Pages/Trending/Trending.js
@@ -2,18 +2,20 @@ import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import SingleContent from '../../SingleContent';
 import "./Trending.css";
+import CustomPagination from '../../CustomPagination';
 
 function Trending() {
     const [content, setContent] = useState([]);
+    const [page, setPage] = useState(1);
     const fetchTrending = async () =>{
-        const {data} = await axios.get("https://api.themoviedb.org/3/trending/all/day?api_key=6c9662a4142c59181dba094fa5a46eb3");
+        const {data} = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=6c9662a4142c59181dba094fa5a46eb3&page=${page}`);
         console.log(data);
         setContent(data.results);
     };
 
     useEffect(() => {
         fetchTrending();
-    },[]);
+    },[page]);
   return <div>
       <span className='pageTitle'>Trending</span>
       <div className="trending">
@@ -32,6 +34,7 @@ function Trending() {
               ))
           }
       </div>
+      <CustomPagination setPage={setPage} />
   </div>;
 }
 
--- trailer-tube/src/SingleContent.js
+++ trailer-tube/src/SingleContent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { img_300, unavailable } from './config/config';
+import Badge from '@material-ui/core/Badge';
 import './SingleContent.css';
 
 function SingleContent({
     id,poster,title,date,media_type,vote_average,
 }) {
   return <div className='media'>
+      <Badge badgeContent={vote_average} color ={vote_average>6? "primary" : "secondary"}/>
       <img className='poster' src ={poster ? `${img_300}/${poster}` : unavailable} alt={title}/>
       <strong className='title'>{title}</strong>
       <span className='subtitle'>{media_type === "tv" ? "TV Series" : "Movie"}
